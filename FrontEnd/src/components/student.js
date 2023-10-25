f09b062c924b3ece45d21128ddf9fc062275e542
21A91A05G9 <21a91a05g9@aec.edu.in>
2023-10-25 15:08:17 +0530

    calender added

--- FrontEnd/src/components/student.js
+++ FrontEnd/src/components/student.js
@@ -36,13 +36,13 @@ export default function Student() {
                 {/* <div className='col-md-1 col-lg-1 col-xl-1 col-sm-1 col-xs-1 '>k</div> */}
                 <div className='col-md-11 col-lg-11 col-xl-11 col-sm-10 col-xs-10 box' >
                     <h1 className='pt-5 pb-3'>Welcoma Back ...</h1>
-                    <div className="row ">
-                    <div className='col-md-3 col-lg-4 col-xl-4 col-sm-4 col-xs-4 m-3' >
+                    <div className="row " >
+                    <div className='col-md-4 col-lg-3 col-xl-4 col-sm-4 col-xs-4 m-3' >
                         <div className='row leaveCnt' > <Counter/> </div>
                     </div>    
 
-                    <div className='col-md-7 col-lg-7 col-xl-7 col-sm-12 col-xs-12 m-3'  >
-                        {/* <div className='row leaveCnt m-3 '> <Calender/> </div> */}
+                    <div className='col-md-7 col-lg-8 col-xl-7 col-sm-12 col-xs-12' >
+                        <div className='row leaveCnt m-3 pt-4'> <Calender/> </div>
                     </div> 
 
                     <div className='row  opt' >
