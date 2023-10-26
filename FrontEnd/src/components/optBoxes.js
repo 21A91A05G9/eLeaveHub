f4ab4ffe7e541290fb433ef4cabfc8a9737b400b
21A91A05G9 <21a91a05g9@aec.edu.in>
2023-10-26 15:57:30 +0530

    student dashboard completed

--- FrontEnd/src/components/optBoxes.js
+++ FrontEnd/src/components/optBoxes.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import './optBoxes.css'
 export default function OptBoxes(props) {
+
   return (
+
     <div>
-       
             <div className='row m-1' >
                 <div className='row m-1' >
-                    <FontAwesomeIcon icon={props.icon} />    
+                    <FontAwesomeIcon icon={props.icon} className="myicon col"/> 
+                    <div className='col'></div>   
                 </div>
                 <button className='btn btn-light'>{props.name}</button>
             </div>
--- FrontEnd/src/components/student.js
+++ FrontEnd/src/components/student.js
@@ -2,7 +2,8 @@ import React from 'react'
 import './student.css'
 import Counter from './counter'
 import { Link } from 'react-router-dom';
-import { faEnvelope,faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleXmark,faCheckSquare ,faChartPie,faListUl} from '@fortawesome/free-solid-svg-icons'
 import OptBoxes from './optBoxes'
 import Calender from './calender'
 export default function Student() {
@@ -10,9 +11,9 @@ export default function Student() {
   return (
     <>
         <div className='container-fluid'>
-            <div className='row dash  mx-1'>
-                <div className='col-md-1 col-lg-1 col-xl-1 col-sm-2 col-xs-2 '>
-                <button class="btn btn-success" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">Menu</button>
+            <div className='row dash'>
+                <div className='col-md-1 col-lg-1 col-xl-1 col-sm-1 col-xs-1 pt-2 menu'>
+                <button class="btn btn-success " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">Menu </button>
 
                 <div class="offcanvas offcanvas-start sidebar" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
                 <div class="offcanvas-header">
@@ -35,20 +36,35 @@ export default function Student() {
               
                 {/* <div className='col-md-1 col-lg-1 col-xl-1 col-sm-1 col-xs-1 '>k</div> */}
                 <div className='col-md-11 col-lg-11 col-xl-11 col-sm-10 col-xs-10 box' >
-                    <h1 className='pt-5 pb-3'>Welcoma Back ...</h1>
+                    <h1 className='pt-4 pb-1 mx-5'>Welcoma Back ...</h1>
                     <div className="row " >
-                    <div className='col-md-4 col-lg-3 col-xl-4 col-sm-4 col-xs-4 m-3' >
+                    <div className='col-md-4 col-lg-4 col-xl-4 col-sm-4 col-xs-4 mx-3' >
                         <div className='row leaveCnt' > <Counter/> </div>
                     </div>    
 
-                    <div className='col-md-7 col-lg-8 col-xl-7 col-sm-12 col-xs-12' >
-                        <div className='row leaveCnt m-3 pt-4'> <Calender/> </div>
+                    <div className='col-md-7 col-lg-7 col-xl-7 col-sm-12 col-xs-12' >
+                        <div className='row leaveCnt m-3 '> <Calender/> </div>
                     </div> 
 
-                    <div className='row  opt' >
-                        <div className='boxes col-md-4'><OptBoxes name="Accept" icon={faCheckSquare}/></div>
-                        <div className='boxes col-md-4'><OptBoxes name="Accept" icon={faCheckSquare}/></div>
-                        <div className='boxes col-md-4'><OptBoxes name="Accept" icon={faCheckSquare}/></div>
+                    <div className='row bottom'>
+                        <div className='col-md-9 col-lg-9 col-xl-9 col-sm-12 col-xs-12'>
+                            <div className='row boxesrow'>
+                                <div className='col-md-4 boxes m-1' >
+                                    <div className='row'><OptBoxes name="Accept" icon={faCheckSquare}/></div>
+                                </div>
+                                <div className=' col-md-4 boxes m-1'>
+                                    <div className='row  '><OptBoxes name="Reject" icon={faCircleXmark}/></div>
+                                </div>
+                                <div className='col-md-4 boxes m-1'>
+                                    <div className='row '><OptBoxes name="Ovarall" icon={faChartPie}/></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-md-2 col-lg-2 col-xl-2 col-sm-12 col-xs-12 row  '>
+                                
+                                <button className='btn btn-light req'><b>Request</b></button>
+                           
+                        </div>
                     </div>
                    
                     
