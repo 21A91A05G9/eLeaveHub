ad1de25a33f555e682b092b07e02fadf98a97c91
21A91A05G9 <21a91a05g9@aec.edu.in>
2023-10-26 18:16:38 +0530

    request btn

--- FrontEnd/src/components/student.js
+++ FrontEnd/src/components/student.js
@@ -12,7 +12,7 @@ export default function Student() {
     <>
         <div className='container-fluid'>
             <div className='row dash'>
-                <div className='col-md-1 col-lg-1 col-xl-1 col-sm-1 col-xs-1 pt-2 menu'>
+                <div className='col-md-1 col-lg-1 col-xl-1 col-sm-1 col-xs-1 pt-4 menu'>
                 <button class="btn btn-success " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">Menu </button>
 
                 <div class="offcanvas offcanvas-start sidebar" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
@@ -36,7 +36,7 @@ export default function Student() {
               
                 {/* <div className='col-md-1 col-lg-1 col-xl-1 col-sm-1 col-xs-1 '>k</div> */}
                 <div className='col-md-11 col-lg-11 col-xl-11 col-sm-10 col-xs-10 box' >
-                    <h1 className='pt-4 pb-1 mx-5'>Welcoma Back ...</h1>
+                    <h1 className='pt-4 pb-1 mx-5 wel'>Welcoma Back ...</h1>
                     <div className="row " >
                     <div className='col-md-4 col-lg-4 col-xl-4 col-sm-4 col-xs-4 mx-3' >
                         <div className='row leaveCnt' > <Counter/> </div>
@@ -46,7 +46,7 @@ export default function Student() {
                         <div className='row leaveCnt m-3 '> <Calender/> </div>
                     </div> 
 
-                    <div className='row bottom'>
+                    <div className='row bottom pt-4'>
                         <div className='col-md-9 col-lg-9 col-xl-9 col-sm-12 col-xs-12'>
                             <div className='row boxesrow'>
                                 <div className='col-md-4 boxes m-1' >
